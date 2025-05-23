f1164b08aa298cb057d1da4d0416b33b54b1a7a9
MAEL <166474301+maelgamer998@users.noreply.github.com>
2025-05-23 20:48:09 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include('includes/header.php'); ?>
+<main>
+  <h1>Olá, eu sou Mael</h1>
+  <p>Desenvolvedor apaixonado por tecnologia e jogos. Veja meus projetos abaixo!</p>
+  <a href="projetos.php">Ver Projetos</a>
+</main>
+<?php include('includes/footer.php'); ?>
